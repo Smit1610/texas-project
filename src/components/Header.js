9d19c6ee0e6a6b7a8d3c437f98f189b90ac97dd4
Charlie <charlie.snow@icloud.com>
2023-08-24 22:16:04 -0400

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './Header.css';
+import PersonIcon from '@mui/icons-material/Person';
+
+function Header() {
+    return (
+        <div className="header">
+            Header
+            {/* logo */}
+            {/* search bar */}
+            <PersonIcon />
+        </div>
+    );
+}
+
+export default Header;
