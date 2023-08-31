7bfb7155d7ebda3bd6e63471ceee52afd2275316
charliemayne <114412078+charliemayne@users.noreply.github.com>
2023-08-31 15:54:44 -0400

    Pulling posts from Firebase

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -2,24 +2,45 @@ import React, { useState, useEffect } from "react";
 import './Feed.css';
 import Post from './Post';
 import CreatePost from "./CreatePost";
-import firestore from '../firebase';
+import db from '../firebase';
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
 
 function Feed() {
-    
+    const [posts, setPosts] = useState([]);
+
+    const fetchPosts = async () => {
+        const postsQuery = query(collection(db, 'posts'), orderBy('timestamp', 'desc'));
+
+        try {
+            const querySnapshot = await getDocs(postsQuery);
+
+            const newData = querySnapshot.docs.map((doc) => ({
+            ...doc.data(),
+            id: doc.id,
+            }));
+
+            setPosts(newData);
+        } catch (error) {
+            console.error('Error fetching posts:', error);
+        }
+    }
+
+    useEffect(() => {
+        fetchPosts();
+    }, []);
+
     return (
         <div className="feed">
             <div className="postColumn">
                 <CreatePost />
-                <Post
-                    title="Hi buddy"
-                    author="Charlie"
-                    content="Hey fellas"
-                />
-                <Post 
-                    title="I have gay thoughts?"
-                    author="Eric"
-                    content="Does anyone else ever feel an urge to bathe with their brother?"
-                />
+                
+                {posts.map(post => (
+                    <Post 
+                        author={post.author}
+                        content={post.content}
+                        title={post.title}
+                    />
+                ))}
             </div>
         </div>
     );
